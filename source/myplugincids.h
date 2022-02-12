65426e0dc64c1589bdcd9da61166b6d0917baf3f
Dr_McFish <61105022+Dr-McFish@users.noreply.github.com>
2022-02-12 19:02:51 +0100

    Initialize repository

---
+++ source/myplugincids.h
@@ -0,0 +1,18 @@
+//------------------------------------------------------------------------
+// Copyright(c) 2022 Dr McFish.
+//------------------------------------------------------------------------
+
+#pragma once
+
+#include "pluginterfaces/base/funknown.h"
+#include "pluginterfaces/vst/vsttypes.h"
+
+namespace McFish {
+//------------------------------------------------------------------------
+static const Steinberg::FUID kPlaceboProcessorUID (0x70B0E0A5, 0xBFE15E80, 0x8B1969D2, 0x48F2D461);
+static const Steinberg::FUID kPlaceboControllerUID (0x574A97BB, 0x0D315AC4, 0x803F4E8C, 0xF749FD8F);
+
+#define PlaceboVST3Category "Fx"
+
+//------------------------------------------------------------------------
+} // namespace McFish
